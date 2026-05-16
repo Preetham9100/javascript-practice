59f6551c69f4900c1553ac09458dece469efc353
Preetham9100 <118185458+Preetham9100@users.noreply.github.com>
2026-05-16 09:44:06 +0000

    ConvertionOfDatatype

---
+++ basics/ConversionOperation.js
@@ -0,0 +1,47 @@
+let age=25
+console.log(typeof age);
+console.log(typeof (age));  //Method ka type hai((inside bracket))
+
+
+
+
+let age1="25"
+console.log(typeof age1); //string
+
+let valueInNumber=Number(age1)
+console.log(typeof valueInNumber); //o/p number
+console.log(valueInNumber);
+
+
+
+
+
+let age2="25abc"
+console.log(typeof age1); //string
+
+let valueInNumber1=Number(age2)
+console.log(typeof valueInNumber1); //number
+console.log(valueInNumber1); //o/p: NaN
+//typeof will become number but there are letters so o/p is NaN(Not a Number)
+
+//"33"=>33
+//"33abc"=NaN
+//true=>1 false=>0
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); //o/p true
+
+//1=>true; 0=>false
+//""=>false
+//"pree"=>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber,stringNumber); //o/p=>string 33
+
+
+
+
