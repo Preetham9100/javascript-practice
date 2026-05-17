6d9f2d2ef6be0cf5d01c657d032c6d0616e08619
Preetham9100 <118185458+Preetham9100@users.noreply.github.com>
2026-05-17 06:12:12 +0000

    typesofmemory

---
+++ basics/memoryTypes.js
@@ -0,0 +1,20 @@
+// stack Memory (Primitive) we get value
+//Heap Memory (Non-Primitive) we get refernce
+
+let myName = "Preetham"
+
+let anotherName = myName
+console.log(anotherName);
+
+anotherName="Singh"
+
+console.log(myName);
+console.log(anotherName);
+
+
+
+let user = {
+    email : "pree@.com",
+    upi : "9100@ybl"
+}
+
